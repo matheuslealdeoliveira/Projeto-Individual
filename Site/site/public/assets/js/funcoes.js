3afabb10b70e0833c2735687f3df2c1e3b803617
matheuslealdeoliveira <matheus.oliveira@sptech.school>
2022-06-02 20:51:36 -0300

    função sair e mostrar nome do usuário

--- Site/site/public/assets/js/funcoes.js
+++ Site/site/public/assets/js/funcoes.js
@@ -1,26 +1,20 @@
-// sessão
 function validarSessao() {
-    // aguardar();
 
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
 
-    var b_usuario = document.getElementById("b_usuario");
+    var nomeusuario = document.getElementById("nomeusuario");
 
     if (email != null && nome != null) {
-        // window.alert(`Seja bem-vindo, ${nome}!`);
-        b_usuario.innerHTML = nome;
+        nomeusuario.innerHTML = nome;
 
-        // finalizarAguardar();
     } else {
         window.location = "../login.html";
     }
 }
 
 function limparSessao() {
-    // aguardar();
     sessionStorage.clear();
-    // finalizarAguardar();
     window.location = "../login.html";
 }
 
--- Site/site/src/database/config.js
+++ Site/site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "f1tech",
-    user: "aluno",
-    password: "sptech",
+    user: "root",
+    password: "#Gf52672537828",
 };
 
 function executar(instrucao) {
